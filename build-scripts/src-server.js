12b28e9a44fe496082ed061636642e531447eddf
Javier Gómez Sasuga <jsasuga@gmail.com>
2018-02-27 22:49:30 -0600

    add webpack and some examples with js and css

--- build-scripts/src-server.js
+++ build-scripts/src-server.js
@@ -1,9 +1,18 @@
-import express from 'express';
-import path from 'path';
-import open from 'open';
+import express from "express";
+import path from "path";
+import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+    require("webpack-dev-middleware")(compiler, {
+        publicPath: config.output.publicPath
+    })
+);
 
 app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "../src/index.html"));
@@ -16,5 +25,3 @@ app.listen(port, err => {
         open("http://localhost:" + port);
     }
 });
-
-
